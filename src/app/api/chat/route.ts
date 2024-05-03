f1752771e0dc2c0080b0f79dd28fe3dd3c2d69b0
Roberto Shimizu <rshimizubr@gmail.com>
2024-05-03 05:35:51 -0300

    update ai sdk

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,10 +1,5 @@
-import OpenAI from 'openai'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
-
-// Create an OpenAI API client (that's edge friendly!)
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
-})
+import { openai } from '@ai-sdk/openai'
+import { StreamingTextResponse, streamText } from 'ai'
 
 // Set the runtime to edge for best performance
 export const runtime = 'edge'
@@ -12,24 +7,10 @@ export const runtime = 'edge'
 export async function POST(req: Request) {
   const { messages } = await req.json()
 
-  const prompt = `You are a helpful AI assistant`
-
-  console.log('Messages length: ', messages.length)
-
-  messages.push({
-    role: 'system',
-    content: prompt
-  })
-
-  // Ask OpenAI for a streaming chat completion given the prompt
-  const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    stream: true,
+  const result = await streamText({
+    model: openai('gpt-3.5-turbo'),
     messages
   })
 
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response)
-  // Respond with the stream
-  return new StreamingTextResponse(stream)
+  return new StreamingTextResponse(result.toAIStream())
 }
