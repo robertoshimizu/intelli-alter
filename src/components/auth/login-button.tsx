da0cdd981abf1731305e190e2ac04c2184381564
Roberto Shimizu <rshimizubr@gmail.com>
2024-05-09 20:35:30 -0300

    add router to LginButton

--- src/components/auth/login-button.tsx
+++ src/components/auth/login-button.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useRouter } from 'next/navigation'
 
 interface LoginButtonProps {
   children: React.ReactNode
@@ -11,8 +12,13 @@ export default function LoginButton({
   mode = 'redirect',
   asChild
 }: LoginButtonProps) {
+  const router = useRouter()
   const onClick = () => {
-    console.log('Login Button clicked')
+    router.push('/auth/login')
+  }
+
+  if (mode === 'modal') {
+    return <span>TODO: Implement modal</span>
   }
   return (
     <span onClick={onClick} className="cursor-pointer">
