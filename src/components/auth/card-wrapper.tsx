54721e9b0e0de6cba038efebcc49df31f38d6c0c
Roberto Shimizu <rshimizubr@gmail.com>
2024-05-09 21:39:29 -0300

    add back button

--- src/components/auth/card-wrapper.tsx
+++ src/components/auth/card-wrapper.tsx
@@ -1,9 +1,10 @@
 'use client'
 
 import React from 'react'
-import { Card, CardContent, CardFooter, CardHeader } from '../ui/card'
+import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
 import { Header } from '@/components/auth/header'
 import { Social } from '@/components/auth/social'
+import { BackButton } from '@/components/auth/back-button'
 
 interface CardWrapperProps {
   children: React.ReactNode
@@ -31,6 +32,9 @@ export const CardWrapper = ({
           <Social />
         </CardFooter>
       )}
+      <CardFooter>
+        <BackButton label={backButtonLabel} href={backButtonHref} />
+      </CardFooter>
     </Card>
   )
 }
