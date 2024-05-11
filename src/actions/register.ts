6abc3233c0453b6995d1f8334237d9ee9ffca51e
Roberto Shimizu <rshimizubr@gmail.com>
2024-05-11 18:22:21 -0300

    add new account creation at register

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,7 +1,9 @@
 'use server'
 
 import { RegisterSchema } from '@/schemas'
+import { db } from '@/lib/db'
 import * as z from 'zod'
+import bcrypt from 'bcrypt'
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values)
@@ -9,6 +11,28 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validatedFields.success) {
     return { error: 'Invalid fields!' }
   }
+
+  const { email, password, name } = validatedFields.data
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  // verify if email is already in use
+  const existingUser = await db.user.findFirst({
+    where: { email }
+  })
+
+  if (existingUser) {
+    return { error: 'Email already in use!' }
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name
+    }
+  })
+
+  // TODO: send email verification token email
 
   return { success: 'New account created!' }
 }
