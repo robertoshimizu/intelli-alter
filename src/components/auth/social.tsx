f97a3b66a446bd110820f6a9c9cc3803672e4d96
Roberto Shimizu <rshimizubr@gmail.com>
2024-05-12 11:42:52 -0300

    remove edge adapter prisma

--- src/components/auth/social.tsx
+++ src/components/auth/social.tsx
@@ -2,15 +2,23 @@
 import { FcGoogle } from 'react-icons/fc'
 import { FaGithub } from 'react-icons/fa'
 import { Button } from '@/components/ui/button'
+import { signIn } from 'next-auth/react'
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes'
+import { on } from 'events'
 
 export const Social = () => {
+  const onClick = (provider: 'google' | 'github') => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT
+    })
+  }
   return (
     <div className="flex items-center w-full gap-x-2">
       <Button
         size="lg"
         variant="outline"
         className="w-full"
-        onClick={() => console.log('Google')}
+        onClick={() => onClick('google')}
       >
         <FcGoogle className="h-5 w-5" />
       </Button>
@@ -18,7 +26,7 @@ export const Social = () => {
         size="lg"
         variant="outline"
         className="w-full"
-        onClick={() => console.log('Github')}
+        onClick={() => onClick('github')}
       >
         <FaGithub className="h-5 w-5" />
       </Button>
