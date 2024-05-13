b8bea81ac0c87576961ccf346e8a91eec5d7eeca
Roberto Shimizu <rshimizubr@gmail.com>
2024-05-13 19:03:34 -0300

    implement prisma accelerate

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 // This boiler plate is to circunvent Nextjs hot reload, to avoid reinitializing the prisma client on every hot reload
 
@@ -6,8 +7,8 @@ declare global {
   var prisma: PrismaClient | undefined
 }
 
-export const db = globalThis.prisma || new PrismaClient()
+export const db = new PrismaClient().$extends(withAccelerate())
 
 console.log('prisma client initialized... at: ', process.env.NODE_ENV)
-
+// @ts-ignore
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = db
