6fe149685cdb4a7257c896e4318be0f3ee82e8ce
Roberto Shimizu <rshimizubr@gmail.com>
2024-05-14 21:28:46 -0300

    cast Extended user

--- src/app/(protected)/client/page.tsx
+++ src/app/(protected)/client/page.tsx
@@ -1,9 +1,10 @@
 'use client'
 import useCurrentUser from '@/hooks/use-current-user'
 import { UserInfo } from '@/components/user-info'
+import { ExtendedUser } from '@/next-auth'
 
 export default function ClientPage() {
-  const user = useCurrentUser()
+  const user = useCurrentUser() as ExtendedUser
   return (
     <div className="flex flex-col gap-y-4 max-w-2xl py-24 mx-auto">
       <UserInfo user={user} label="📱 Client component" />
