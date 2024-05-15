a68d7f3b3330832913505493c90646cc3adc81d2
Roberto Shimizu <rshimizubr@gmail.com>
2024-05-15 15:26:21 -0300

    add update name on settings

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,10 +1,27 @@
-import { auth } from '@/auth'
+'use client'
 
-export default async function Page() {
-  const session = await auth()
+import { settings } from '@/actions/settings'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { useTransition } from 'react'
+
+export default function SettingsPage() {
+  const [isPending, startTransition] = useTransition()
+  const onClick = () => {
+    startTransition(() => {
+      settings({ name: 'New Name!' })
+    })
+  }
   return (
-    <div className="flex flex-col gap-y-4 max-w-2xl py-24 mx-auto">
-      <div>{JSON.stringify(session)}</div>
-    </div>
+    <Card className="w-[600px]">
+      <CardHeader>
+        <p className="text-2xl font-semibold text-center">⚙️ Settings</p>
+      </CardHeader>
+      <CardContent>
+        <Button disabled={isPending} onClick={onClick} className="w-full">
+          Update Name
+        </Button>
+      </CardContent>
+    </Card>
   )
 }
