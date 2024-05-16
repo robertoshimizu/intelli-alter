6a0fc3125c546fe63bcba1a801286e91b8d2bcde
Roberto Shimizu <rshimizubr@gmail.com>
2024-05-16 14:38:52 -0300

    Sidebar components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+import { Providers } from '@/components/providers'
 import { Toaster } from '@/components/ui/sonner'
 import './globals.css'
 import { cn } from '@/lib/utils'
@@ -29,12 +30,19 @@ export default async function RootLayout({
       <html lang="en" suppressHydrationWarning>
         <body className={cn('font-sans antialiased', inter)}>
           <Toaster position="top-center" />
-          <div className="flex flex-col min-h-screen ">
-            <Header />
-            <main className="flex flex-col flex-1 bg-muted/50 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-100 to-blue-600">
-              {children}
-            </main>
-          </div>
+          <Providers
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="flex flex-col min-h-screen ">
+              <Header />
+              <main className="flex flex-col flex-1 bg-muted/50 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-100 to-blue-600">
+                {children}
+              </main>
+            </div>
+          </Providers>
         </body>
       </html>
     </ClerkProvider>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,10 +5,12 @@ import { cn } from '@/lib/utils'
 
 import { Button, buttonVariants } from '@/components/ui/button'
 import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { IconSeparator } from '@/components/ui/icons'
+import { SideMenu } from '@/components/header-side-menu'
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b border-sky-800 shrink-0 bg-gradient-to-b from-sky-500  to-blue-800 backdrop-blur-xl ">
+    <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
       <div className="flex items-center">
         <SignedOut>
           <Button variant="link" asChild className="-ml-2">
@@ -16,8 +18,10 @@ export function Header() {
           </Button>
         </SignedOut>
         <SignedIn>
+          <SideMenu />
+          <IconSeparator className="size-6 text-muted-foreground/50" />
           {/* Mount the UserButton component */}
-          Menu
+          UserMenu
         </SignedIn>
       </div>
       <div className="flex items-center justify-end space-x-2">
