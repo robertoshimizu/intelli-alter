26072bb788b30d0c4d8727a8190e540a6fd7cc86
Roberto Shimizu <rshimizubr@gmail.com>
2024-05-19 22:55:30 -0300

    add paths

--- src/app/api/get-users/route.ts
+++ src/app/api/get-users/route.ts
@@ -1,21 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server'
 /**
  * @swagger
- * /api/get-users:
- *  get:
- *   description: Returns the hello world message
- *   parameters:
- *       - name: query
- *         in: query
- *         required: true
- *         type: string
- *   responses:
- *    200:
- *      description: Hello, world!
- *    400:
- *     description: Bad request
- *   500:
- *    description: Internal Server Error
+ *  paths:
+ *    /api/get-users:
+ *      get:
+ *        description: Returns the hello world message
+ *        parameters:
+ *          - name: query
+ *            in: query
+ *            required: true
+ *            type: string
+ *        responses:
+ *          200:
+ *            description: Hello, world!
+ *          400:
+ *            description: Bad request
+ *          500:
+ *            description: Internal Server Error
  */
 
 export async function GET(req: NextRequest) {
