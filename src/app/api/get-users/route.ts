39e2255abe803846de574640c2fb9d8bf45d0a61
Roberto Shimizu <rshimizubr@gmail.com>
2024-05-19 22:34:47 -0300

    add swagger

---
+++ src/app/api/get-users/route.ts
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from 'next/server'
+/**
+ * @swagger
+ * /api/get-users:
+ *  get:
+ *   description: Returns the hello world message
+ *   parameters:
+ *       - name: query
+ *         in: query
+ *         required: true
+ *         type: string
+ *   responses:
+ *    200:
+ *      description: Hello, world!
+ *    400:
+ *     description: Bad request
+ *   500:
+ *    description: Internal Server Error
+ */
+
+export async function GET(req: NextRequest) {
+  try {
+    console.log(req.nextUrl.searchParams)
+    console.log('Hello, world!')
+    return NextResponse.json(
+      { message: 'Hello world malandro!' },
+      { status: 200 }
+    )
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    )
+  }
+}
