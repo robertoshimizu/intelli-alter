a7326f79661d1d43a598d23415ed579151015d47
Roberto Shimizu <rshimizubr@gmail.com>
2024-05-24 09:09:12 -0300

    add python function

---
+++ src/app/api/hello-world/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from 'next/server'
+/**
+ * @swagger
+ *  paths:
+ *    /api/hello-world:
+ *      get:
+ *        description: Returns hello world
+ *        parameters: none
+ *        responses:
+ *          200:
+ *            description: Success
+ *          400:
+ *            description: Bad request
+ *          500:
+ *            description: Internal Server Error
+ */
+
+export async function GET(req: NextRequest) {
+  try {
+    console.log('Hello, world!')
+    return NextResponse.json(
+      { message: 'Hello world capial!' },
+      { status: 200 }
+    )
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    )
+  }
+}
