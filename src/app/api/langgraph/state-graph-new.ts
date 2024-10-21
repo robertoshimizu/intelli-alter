6f94216cb5d2b873099c4ee3039d626fa5658b25
Roberto Shimizu <rshimizubr@gmail.com>
2024-10-21 19:30:52 -0300

    base graph

--- src/app/api/langgraph/state-graph-new.ts
+++ src/app/api/langgraph/state-graph-new.ts
@@ -1,122 +1,73 @@
-import { ChatOpenAI } from '@langchain/openai'
+import { ToolNode } from '@langchain/langgraph/prebuilt'
 import {
-  Annotation,
   END,
+  MemorySaver,
   MessagesAnnotation,
   START,
   StateGraph
 } from '@langchain/langgraph'
-import { ToolNode } from '@langchain/langgraph/prebuilt'
-import { convertToOpenAIFunction } from '@langchain/core/utils/function_calling'
+import { AIMessage, BaseMessage } from '@langchain/core/messages'
+import { ChatOpenAI } from '@langchain/openai'
 import { TavilySearchResults } from '@langchain/community/tools/tavily_search'
-import { StructuredTool } from '@langchain/core/tools'
 
-// Set up the tools
-const tools = [
-  new TavilySearchResults({ maxResults: 1 })
-] as Array<StructuredTool>
-
-// Convert tools to OpenAI functions
-const toolsAsOpenAIFunctions = tools.map((tool) =>
-  convertToOpenAIFunction(tool)
-)
-
-// Set up the model
-const model = new ChatOpenAI({
+const llm = new ChatOpenAI({
   model: 'gpt-4o-mini',
-  temperature: 0,
-  streaming: true
-}).bind({
-  functions: toolsAsOpenAIFunctions
+  temperature: 0
 })
 
-// Define the agent state using Annotation
-const agentState = Annotation.Root({
-  ...MessagesAnnotation.spec,
-  memory: Annotation<any | null>({
-    reducer: (x?: any | null, y?: any | null) => y ?? x,
-    default: () => null
-  }),
-  topic: Annotation<any | null>({
-    reducer: (x?: any | null, y?: any | null) => y ?? x,
-    default: () => null
-  }),
-  input: Annotation<any | null>({
-    reducer: (x?: any | null, y?: any | null) => y ?? x,
-    default: () => ''
-  })
+const webSearchTool = new TavilySearchResults({
+  maxResults: 4
 })
+const tools = [webSearchTool]
 
-// Type for agent state
-type AgentState = typeof agentState.State
+const toolNode = new ToolNode(tools)
 
-// Define the function that determines whether to continue or not
+const callModel = async (state: typeof MessagesAnnotation.State) => {
+  console.log('Welcome - This Intro Agent')
+  const { messages } = state
 
-const shouldContinue = (state: AgentState) => {
+  const llmWithTools = llm.bindTools(tools)
+  const result = await llmWithTools.invoke(messages)
+  return { messages: [result] }
+}
+
+const shouldContinue = (state: typeof MessagesAnnotation.State) => {
   const { messages } = state
 
   const lastMessage = messages[messages.length - 1]
-
-  //console.log('\n ShouldContinue lastMessage:', lastMessage)
-
-  // If there is a function call, we continue to tools
   if (
-    'function_call' in lastMessage.additional_kwargs &&
-    lastMessage.additional_kwargs.function_call
+    lastMessage._getType() !== 'ai' ||
+    !(lastMessage as AIMessage).tool_calls?.length
   ) {
-    return 'tools'
+    // LLM did not call any tools, or it's not an AI message, so we should end.
+    console.log('Ending workflow')
+    return END
   }
-  // Otherwise, we end
-  return 'end'
-  // if (
-  //   lastMessage._getType() !== 'ai' ||
-  //   !(lastMessage as AIMessage).tool_calls?.length
-  // ) {
-  //   // LLM did not call any tools, or it's not an AI message, so we should end.
-  //   return 'end'
-  // }
-  // return 'tools'
+  return 'tools'
 }
 
-// Define the function that calls the model
-async function callModel(state: AgentState) {
-  const { messages } = state
+/**
+ * MessagesAnnotation is a pre-built state annotation imported from @langchain/langgraph.
+ * It is the same as the following annotation:
+ *
+ * ```typescript
+ * const MessagesAnnotation = Annotation.Root({
+ *   messages: Annotation<BaseMessage[]>({
+ *     reducer: messagesStateReducer,
+ *     default: () => [systemMessage],
+ *   }),
+ * });
+ * ```
+ */
+const workflow = new StateGraph(MessagesAnnotation)
+  .addNode('agent', callModel)
+  .addEdge(START, 'agent')
+  .addNode('tools', toolNode)
+  .addEdge('tools', 'agent')
+  .addConditionalEdges('agent', shouldContinue, ['tools', END])
 
-  const lastMessage = messages[messages.length - 1]
-
-  //console.log('\n CallModel lastMessage:', lastMessage)
-
-  const response = await model.invoke([
-    {
-      role: 'system',
-      content: `You are a helpful assistant. For every question, research, get the results, reflect on the question to craft the response, and then respond the best way you can.`
-    },
-    ...state.messages
-  ])
-
-  //console.log('\n CallModel response:', response)
-
-  // Return the updated messages
-  return { messages: response }
-}
-
-// Create an instance of ToolNode
-const toolNodeForGraph = new ToolNode(tools)
-
-// Define the graph
-const workflow = new StateGraph(agentState)
-  // Add nodes
-  .addNode('callModel', callModel)
-  .addNode('tools', toolNodeForGraph)
-  // Set the entry point
-  .addEdge(START, 'callModel')
-  // Add conditional edges
-  .addConditionalEdges('callModel', shouldContinue, {
-    tools: 'tools',
-    end: END
-  })
-  // Add edge from tools to callModel
-  .addEdge('tools', 'callModel')
-
-// Compile the graph
-export const app = workflow.compile()
+export const app = workflow.compile({
+  // The LangGraph Studio/Cloud API will automatically add a checkpointer
+  // only uncomment if running locally
+  checkpointer: new MemorySaver()
+})
